49451258a17df9996aec215e0fc3268e640279af
Mario Benitez <nadamasfacil.mb@gmail.com>
2022-08-26 18:22:09 -0600

    Tarea 08 Algoritmos II

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,6 +6,24 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) return array;
+  let arrayLength=array.length;
+  let pivote=Math.floor(Math.random()*arrayLength);
+  let arrayPivote=array[pivote];
+  let arrayLeft=[];
+  let arrayRight=[];
+  for (let i=0; i < arrayLength; i++)
+    if (i !== pivote){
+        if (array[i] <= arrayPivote) arrayLeft.push(array[i]);
+        else arrayRight.push(array[i]);
+  }
+  arrayLeft=quickSort(arrayLeft);
+  arrayRight=quickSort(arrayRight);
+  array=[];
+  array=array.concat(arrayLeft);
+  array.push(arrayPivote);
+  array=array.concat(arrayRight);
+  return array;
 
 }
 
@@ -14,6 +32,32 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) return array;
+  let arrayLeft=[];
+  let arrayRight=[];
+  let arrayLength=array.length;
+  let temp=0, indInd=0;
+  let numMid=Math.floor(arrayLength/2)-1;
+  for (let i=0; i < arrayLength; i++){
+      if (i <= numMid) arrayLeft.push(array[i]);
+      else arrayRight.push(array[i]);
+  }
+  arrayLeft=mergeSort(arrayLeft);
+  arrayRight=mergeSort(arrayRight);
+  for (let i = 0; i < arrayRight.length; i++){
+      arrayLeft.push(arrayRight[i]);
+      indInd=arrayLeft.length;
+      while (indInd > 0){   
+        if (arrayLeft[indInd-1] > arrayLeft[indInd]){
+            temp=arrayLeft[indInd-1];
+            arrayLeft[indInd-1]=arrayLeft[indInd];
+            arrayLeft[indInd]=temp;
+        }
+        indInd--  
+      }
+  }
+  array=arrayLeft;
+  return array;
 
 }
 
